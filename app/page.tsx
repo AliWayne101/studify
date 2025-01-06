a25545de675aa2c3d51f083b3e58d4c44f6a38fa
AliWayne101 <alimalikwayne@gmail.com>
2025-01-06 23:51:48 +0500

    first upl

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './css/home.scss'
+
+const Home = () => {
+  return (
+    <main>
+      <div className="hero">
+        <h1>Studify</h1>
+        <p>Stay connected to your child&apos;s academic journey with real-time updates. Empower their success with instant access to grades, attendance, and progress.</p>
+      </div>
+    </main>
+  )
+}
+
+export default Home
