842800a138d91f728044adfa6ae9447d2b409980
AliMalik101 <alimalikwayne@gmail.com>
2025-01-07 18:10:25 +0500

    Socials and Footer

--- app/page.tsx
+++ app/page.tsx
@@ -5,57 +5,59 @@ import Footer from './sections/footer'
 
 const Home = () => {
   return (
-    <main>
-      <div className="hero">
-        <h1>Studify</h1>
-        <p>Stay connected to your child&apos;s academic journey with real-time updates. Empower their success with instant access to grades, attendance, and progress.</p>
-      </div>
-      <div className="cards">
-        <div className="cards-cont">
-          <h1>Title</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur necessitatibus omnis saepe. Doloribus ad, nihil sequi dolores soluta odit amet, ipsa iste placeat, assumenda adipisci perspiciatis voluptates quam dolorem possimus!</p>
-          <h3>Features</h3>
-          <ul>
-            <li>One</li>
-            <li>Two</li>
-            <li>Three</li>
-            <li>Four</li>
-          </ul>
-          <div className="cards-cont-cta">
-            <Link href={'#'} className='cta bg'>Login</Link>
+    <>
+      <main>
+        <div className="hero">
+          <h1>Studify</h1>
+          <p>Stay connected to your child&apos;s academic journey with real-time updates. Empower their success with instant access to grades, attendance, and progress.</p>
+        </div>
+        <div className="cards">
+          <div className="cards-cont">
+            <h2>Title</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur necessitatibus omnis saepe. Doloribus ad, nihil sequi dolores soluta odit amet, ipsa iste placeat, assumenda adipisci perspiciatis voluptates quam dolorem possimus!</p>
+            <h3>Features</h3>
+            <ul>
+              <li>One</li>
+              <li>Two</li>
+              <li>Three</li>
+              <li>Four</li>
+            </ul>
+            <div className="cards-cont-cta">
+              <Link href={'#'} className='cta bg'>Login</Link>
+            </div>
+          </div>
+          <div className="cards-cont">
+            <h2>Title</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur necessitatibus omnis saepe. Doloribus ad, nihil sequi dolores soluta odit amet, ipsa iste placeat, assumenda adipisci perspiciatis voluptates quam dolorem possimus!</p>
+            <h3>Features</h3>
+            <ul>
+              <li>One</li>
+              <li>Two</li>
+              <li>Three</li>
+              <li>Four</li>
+            </ul>
+            <div className="cards-cont-cta">
+              <Link href={'#'} className='cta bg'>Login</Link>
+            </div>
+          </div>
+          <div className="cards-cont">
+            <h2>Title</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur necessitatibus omnis saepe. Doloribus ad, nihil sequi dolores soluta odit amet, ipsa iste placeat, assumenda adipisci perspiciatis voluptates quam dolorem possimus!</p>
+            <h3>Features</h3>
+            <ul>
+              <li>One</li>
+              <li>Two</li>
+              <li>Three</li>
+              <li>Four</li>
+            </ul>
+            <div className="cards-cont-cta">
+              <Link href={'#'} className='cta bg'>Login</Link>
+            </div>
           </div>
         </div>
-        <div className="cards-cont">
-          <h1>Title</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur necessitatibus omnis saepe. Doloribus ad, nihil sequi dolores soluta odit amet, ipsa iste placeat, assumenda adipisci perspiciatis voluptates quam dolorem possimus!</p>
-          <h3>Features</h3>
-          <ul>
-            <li>One</li>
-            <li>Two</li>
-            <li>Three</li>
-            <li>Four</li>
-          </ul>
-          <div className="cards-cont-cta">
-            <Link href={'#'} className='cta bg'>Login</Link>
-          </div>
-        </div>
-        <div className="cards-cont">
-          <h1>Title</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur necessitatibus omnis saepe. Doloribus ad, nihil sequi dolores soluta odit amet, ipsa iste placeat, assumenda adipisci perspiciatis voluptates quam dolorem possimus!</p>
-          <h3>Features</h3>
-          <ul>
-            <li>One</li>
-            <li>Two</li>
-            <li>Three</li>
-            <li>Four</li>
-          </ul>
-          <div className="cards-cont-cta">
-            <Link href={'#'} className='cta bg'>Login</Link>
-          </div>
-        </div>
-      </div>
+      </main>
       <Footer />
-    </main>
+    </>
   )
 }
 
--- app/sections/footer.tsx
+++ app/sections/footer.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import "../css/sections/footer.scss";
+import Link from 'next/link';
 
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="footer-in">Segment 1</div>
-      <div className="footer-in">Segment 2</div>
-      <div className="footer-in">Segment 3</div>
+      <div className="footer-in">
+        <h1>Links</h1>
+        <ul>
+          <li><Link href={'#'}>Link 1</Link></li>
+          <li><Link href={'#'}>Link 2</Link></li>
+          <li><Link href={'#'}>Link 3</Link></li>
+          <li><Link href={'#'}>Link 4</Link></li>
+          <li><Link href={'#'}>Link 5</Link></li>
+        </ul>
+      </div>
+      <div className="footer-in">
+        Copyright © 2023 Studify. All rights reserved.<br />
+        Site Developed and Hosted by <Link href='https://waynedev.vercel.app' target='_blank'><b>Wayne Development</b></Link>
+      </div>
+      <div className="footer-in">
+        <h1>Socials</h1>
+      </div>
     </div>
   )
 }
