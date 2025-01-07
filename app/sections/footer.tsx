975ffbc69873f3d576815d21e2aa2094f52fbd2b
AliMalik101 <alimalikwayne@gmail.com>
2025-01-07 17:09:26 +0500

    footer upl

--- app/sections/footer.tsx
+++ app/sections/footer.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import "../css/sections/footer.scss";
 
 const Footer = () => {
   return (
-    <div>This is footer</div>
+    <div className="footer">
+      <div className="footer-in">Segment 1</div>
+      <div className="footer-in">Segment 2</div>
+      <div className="footer-in">Segment 3</div>
+    </div>
   )
 }
 
