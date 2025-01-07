68d560ac28e8d2f3c0d8fb01a08426d80155dd4a
AliMalik101 <alimalikwayne@gmail.com>
2025-01-07 16:56:37 +0500

    Added Cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './css/home.scss'
+import Link from 'next/link'
+import Footer from './sections/footer'
 
 const Home = () => {
   return (
@@ -8,6 +10,51 @@ const Home = () => {
         <h1>Studify</h1>
         <p>Stay connected to your child&apos;s academic journey with real-time updates. Empower their success with instant access to grades, attendance, and progress.</p>
       </div>
+      <div className="cards">
+        <div className="cards-cont">
+          <h1>Title</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur necessitatibus omnis saepe. Doloribus ad, nihil sequi dolores soluta odit amet, ipsa iste placeat, assumenda adipisci perspiciatis voluptates quam dolorem possimus!</p>
+          <h3>Features</h3>
+          <ul>
+            <li>One</li>
+            <li>Two</li>
+            <li>Three</li>
+            <li>Four</li>
+          </ul>
+          <div className="cards-cont-cta">
+            <Link href={'#'} className='cta bg'>Login</Link>
+          </div>
+        </div>
+        <div className="cards-cont">
+          <h1>Title</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur necessitatibus omnis saepe. Doloribus ad, nihil sequi dolores soluta odit amet, ipsa iste placeat, assumenda adipisci perspiciatis voluptates quam dolorem possimus!</p>
+          <h3>Features</h3>
+          <ul>
+            <li>One</li>
+            <li>Two</li>
+            <li>Three</li>
+            <li>Four</li>
+          </ul>
+          <div className="cards-cont-cta">
+            <Link href={'#'} className='cta bg'>Login</Link>
+          </div>
+        </div>
+        <div className="cards-cont">
+          <h1>Title</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur necessitatibus omnis saepe. Doloribus ad, nihil sequi dolores soluta odit amet, ipsa iste placeat, assumenda adipisci perspiciatis voluptates quam dolorem possimus!</p>
+          <h3>Features</h3>
+          <ul>
+            <li>One</li>
+            <li>Two</li>
+            <li>Three</li>
+            <li>Four</li>
+          </ul>
+          <div className="cards-cont-cta">
+            <Link href={'#'} className='cta bg'>Login</Link>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </main>
   )
 }
