719208ad7a3d66069d9274e0b66d8ed4328a1a02
AliWayne101 <alimalikwayne@gmail.com>
2025-01-22 14:30:25 +0500

    Cleanup

--- app/sections/Authority/AuthList.tsx
+++ app/sections/Authority/AuthList.tsx
@@ -2,8 +2,10 @@
 import React, { useEffect, useState } from 'react';
 import "../../css/sections/Authority/basic.scss";
 import { SessionProps } from '@/interfaces';
-import List from './List';
 import { IUserInfo } from '@/schema/userinfo';
+import "../../css/sections/Authority/list.scss";
+import Image from 'next/image';
+import { getImageLink } from '@/utils';
 
 const AuthList = ({ session }: SessionProps) => {
     const [isError, setIsError] = useState<string | null>(null);
@@ -65,9 +67,27 @@ const AuthList = ({ session }: SessionProps) => {
     }, [session.user.role])
 
     return (
-        <List Title={listTitle} List={userInfo}/>
-        // isAuthorized(session.user.role, ["SU", "HU"]) ? (
-        // ) : null
+        <div className="list">
+            <h2>{listTitle}</h2>
+            <div className="list-cards">
+                {userInfo?.map((doc, index) => (
+                    <div className="list-cards-card" key={index}>
+                        <div className="list-cards-card-image">
+                            <Image height={1000} width={1000} src={getImageLink(doc.Image)} alt={doc.Name} />
+                        </div>
+                        <ul>
+                            <li><span>Name:</span> {doc.Name}</li>
+                            <li><span>Role:</span> {doc.Role}</li>
+                            <li><span>CNIC:</span> {doc.CNIC}</li>
+                            <li><span>Phone:</span> {doc.Phone}</li>
+                            <li><span>DOB:</span> {new Date(doc.DOB.toString()).toLocaleDateString('en-GB')}</li>
+                            <li><span>Joining Date:</span> {new Date(doc.JoinedOn.toString()).toLocaleDateString('en-GB')}</li>
+                            <li>Address <br /> <small>{doc.Address}</small></li>
+                        </ul>
+                    </div>
+                ))}
+            </div>
+        </div>
     );
 };
 
